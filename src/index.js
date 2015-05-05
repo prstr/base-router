f56f32dbd0c460a34775c2484daefd315d6dd4fe
Boris Okunskiy <i@savant.pro>
2015-05-05 15:10:14 +0300

    Added serving json pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 "use strict";
 
 var express = require('express')
+  , async = require('async')
+  , fs = require('fs-extra')
   , path = require('path')
   , fallback = require('fallback')
   , debug = require('debug')('prostore:site');
 
 var router = module.exports = exports = new express.Router();
+
+/**
+ * Маршруты для работы с редактируемыми страницами.
+ *
+ * В `res.render` должен находиться шаблонизатор ProStore.
+ * В `res.locals.root` должен лежать путь к корню магазина.
+ */
+router.get('/*', function(req, res, next) {
+  var id = req.params[0].replace(/\.html$/, '');
+  var file = path.join(res.locals.root, 'pages', id + '.json');
+  debug('Trying %s', file);
+  fs.readJsonFile(file, 'utf-8', function(err, page) {
+    if (!page) return next();
+    // Render page blocks
+    async.map(page.blocks || [], function(block, cb) {
+      res.render('blocks/' + (block.type || 'default') + '.html', {
+        block: block
+      }, function(err, html) {
+        if (err) return cb(err);
+        block.html = html;
+        cb(null, block);
+      });
+    }, function(err, blocks) {
+      if (err) return next(err);
+      page.id = id;
+      page.html = blocks.map(function(block) { return block.html }).join('\n');
+      res.render(page.template || 'pages/default.html', {
+        page: page
+      });
+    });
+  });
+});
 
 /**
  * Маршруты для отрисовки страниц сайта с помощью встроенного шаблонизатора.
@@ -23,6 +57,8 @@ var router = module.exports = exports = new express.Router();
  *
  * Поиск шаблонов осуществляется в директории `site` папки с шаблонами
  * (т.е. `<storeRoot>/templates/site`).
+ *
+ * В `res.render` должен находиться шаблонизатор ProStore.
  */
 router.get('/*', function(req, res, next) {
   var ext = path.extname(req.url);
